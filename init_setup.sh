5eef852bcb73212acd72615cb49dec0d9651baf4
Nithin Pradeep <nithinpradeep@Nithins-MacBook-Pro.local>
2024-01-08 22:09:10 -0800

    Bash script ran

--- init_setup.sh
+++ init_setup.sh
@@ -3,14 +3,21 @@ echo [$(date)]: "creating environment"
 conda create --prefix ./env python=3.8 -y
 echo [$(date)]: "activate environment"
 source activate ./env
+echo [$(date)]: "Create folder and file structure"
+
+for DIR in data_ingestion data_preparation data_validation model model_evaluation model_trainer
+do 
+    echo "Creating", src/"NER_"$DIR
+    mkdir -p src/"NER_"$DIR
+    echo "Creating __init__.py inside above folders"
+    touch src/"NER_"$DIR/"__init__.py" src/"NER_"$DIR/$DIR".py"
+    echo "from src.NER_"$DIR"."$DIR" import *" > "src/NER_"$DIR/"__init__.py"
+done
+
 echo [$(date)]: "install requirements"
-pip install -r requirements.txt
-echo [$(date)]: "export conda environment"
-conda env export > conda.yaml
-echo "# ${PWD}" > README.md
-echo [$(date)]: "first commit"
-git add .
-git commit -m "first commit"
+pip install -r requirements.txt -q
+echo [$(date)]: "install pytorch dependency"
+pip3 install torch -q
 echo [$(date)]: "END"
 
 # to remove everything -
